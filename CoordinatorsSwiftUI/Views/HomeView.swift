13e58898dc68780fa855b48cd7751c730fa04a0a
makkina <efe@osazee.com>
2024-03-03 23:18:57 +0100

    Rearrange Button in code HomeView

--- CoordinatorsSwiftUI/Views/HomeView.swift
+++ CoordinatorsSwiftUI/Views/HomeView.swift
@@ -10,23 +10,18 @@ struct HomeView: View {
                 .resizable()
                 .frame(width: 60, height: 50)
             Text("Home")
-            Button {
-                coordinator.push(page: .childViewOne)
-            } label: {
+            
+            Button { coordinator.push(page: .childViewOne) } label: {
                 Text("Navigate to Child View One")
             }
             
             Spacer()
             
-            Button {
-                coordinator.present(sheet: .alertView)
-            } label: {
+            Button { coordinator.present(sheet: .alertView) } label: {
                 Text("Present to Alert View")
             }
             
-            Button {
-                coordinator.present(fullScreenCover: .warningView)
-            } label: {
+            Button { coordinator.present(fullScreenCover: .warningView) } label: {
                 Text("Present to Warning View")
             }
         }
