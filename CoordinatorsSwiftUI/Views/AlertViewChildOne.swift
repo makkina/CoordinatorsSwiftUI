aa6d63f71874ff06f76ce2f18f7c0ab1d4d2ba9a
makkina <efe@osazee.com>
2024-03-04 04:36:54 +0100

    Added solution to dismissSheet from inside the context of a different childCoordinator

---
+++ CoordinatorsSwiftUI/Views/AlertViewChildOne.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct AlertViewChildOne: View {
+    @EnvironmentObject var childCoordinator: ApplicationCoordinator
+    
+    var body: some View {
+        VStack(spacing: 25) {
+            Image(systemName: "m1.button.horizontal.fill").font(.largeTitle)
+            
+            /// @action: Close Modal
+            Button { childCoordinator.closeSheet?.toggle() } label: {
+                (Text("Close Entire modal &nbsp;") + Text(Image(systemName: "xmark.circle.fill")))
+            }
+            
+            HStack(spacing: 50) {
+                /// @action: Go Back
+                Button { childCoordinator.pop() } label: {
+                    (Text(Image(systemName: "arrow.backward")) + Text("&nbsp; Back"))
+                }
+                /// @action: Go To Next Screen
+                Button { childCoordinator.push(page: .alertViewChildTwo) } label: {
+                    (Text("Next &nbsp;") + Text(Image(systemName: "arrow.forward")))
+                }
+            }
+            .navigationTitle("Alert View Child One")
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        AlertViewChildOne().environmentObject(ApplicationCoordinator())
+    }
+}
---
+++ CoordinatorsSwiftUI/Views/AlertViewChildTwo.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+struct AlertViewChildTwo: View {
+    @EnvironmentObject var childCoordinator: ApplicationCoordinator
+
+    var body: some View {
+        VStack(spacing: 25) {
+            Image(systemName: "m2.button.horizontal.fill").font(.largeTitle)
+            
+            /// @action: Close Modal
+            Button { childCoordinator.closeSheet?.toggle() } label: {
+                (Text("Close Entire modal &nbsp;") + Text(Image(systemName: "xmark.circle.fill")))
+            }
+
+            HStack(spacing: 50) {
+                /// @action: Go Back
+                Button { childCoordinator.pop() } label: {
+                    (Text(Image(systemName: "arrow.backward")) + Text("&nbsp; Back"))
+                }
+                .padding(.leading, -100)
+            }
+        }
+        .navigationTitle("Alert View Child Two")
+    }
+}
+
+#Preview {
+    NavigationStack {
+        AlertViewChildOne().environmentObject(ApplicationCoordinator())
+    }
+}
