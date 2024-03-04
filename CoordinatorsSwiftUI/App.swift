45019a2a878a6c87f8771bc28fbda7d12293be34
makkina <efe@osazee.com>
2024-03-04 20:05:06 +0100

    Updated next steps in App.swift

--- CoordinatorsSwiftUI/App.swift
+++ CoordinatorsSwiftUI/App.swift
@@ -10,7 +10,7 @@ struct ProjectNavigatorApp: App {
 }
 
 /**
- @TODO:
+ @NEXT:
  
  1.  Use Coordinators to `close fullScreenCover`
  2.  Use Coordinators to `show` Views (~ push full view without animation)
@@ -18,4 +18,7 @@ struct ProjectNavigatorApp: App {
         3.1. Page
         3.2. SheetType
         3.3. FullScreenCoverType
+ 4. Make injectable `$index` variable to reuse the popUpViews(!) e.g. coordinator.present(sheet: .alertView(index: .profileView))
+        4.1 Rename alertView to sheetViewCoordinatorWrapperView() / .sheetCoordinatorWrapperView / .sheetViewCoordinator
+        4.2 Rename warningView to fullScreenCoverCoordinatorWrapperView() / / .sheetCoordinatorWrapperView / .fullScreenCoverViewCoordinator
  */
