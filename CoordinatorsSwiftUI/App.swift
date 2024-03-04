b3ff48e7fbc6170258e9bc7b752dc43cec4a64d6
makkina <efe@osazee.com>
2024-03-04 15:02:50 +0100

    CoordinatorsSwiftUI

--- CoordinatorsSwiftUI/App.swift
+++ CoordinatorsSwiftUI/App.swift
@@ -14,4 +14,8 @@ struct ProjectNavigatorApp: App {
  
  1.  Use Coordinators to `close fullScreenCover`
  2.  Use Coordinators to `show` Views (~ push full view without animation)
+ 3.  Single Hashable Type to manage NavigationPages / Routes
+        3.1. Page
+        3.2. SheetType
+        3.3. FullScreenCoverType
  */
